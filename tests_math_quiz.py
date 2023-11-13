c9203bbed71d815d739ba9d0562c23abcb00145c
Megh Vora <mail.megh1607@gmail.com>
2023-11-13 18:44:24 +0100

    Add unit tests

---
+++ tests_math_quiz.py
@@ -0,0 +1,35 @@
+import unittest
+from math_quiz import function_A, function_B, function_C
+
+
+class TestMathGame(unittest.TestCase):
+
+    def test_function_A(self):
+        # Test if random numbers generated are within the specified range
+        min_val = 1
+        max_val = 10
+        for _ in range(1000):  # Test a large number of random values
+            rand_num = function_A(min_val, max_val)
+            self.assertTrue(min_val <= rand_num <= max_val)
+
+    def test_function_B(self):
+        # Test if the generated operator is one of '+', '-', or '*'
+        valid_operators = ['+', '-', '*']
+        for _ in range(1000):  # Test a large number of random values
+            operator = function_B()
+            self.assertIn(operator, valid_operators)
+
+    def test_function_C(self):
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (3, 8, '*', '3 * 8', 24),
+            # Add more test cases as needed
+        ]
+
+        for n1, n2, o, expected_problem, expected_answer in test_cases:
+            problem, answer = function_C(n1, n2, o)
+            self.assertEqual(problem, expected_problem)
+            self.assertEqual(answer, expected_answer)
+
+if __name__ == "__main__":
+    unittest.main()
